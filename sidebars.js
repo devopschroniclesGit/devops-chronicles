94f00046de4533142048fece0f3d9f78c5413d6b
devopschroniclesgit <github@devopschronicles.com>
2026-04-30 22:59:53 +0200

    docs: fix image reference

--- sidebars.js
+++ sidebars.js
@@ -39,9 +39,9 @@ const sidebars = {
       collapsed: false,
       items: [
         'projects/finpay/finpay-api',
-        //'projects/case-studies/index',
-       // 'projects/security/index',
-       // 'projects/monitoring/index',
+        'projects/case-studies/index',
+        'projects/security/index',
+        'projects/monitoring/index',
       ],
     },
     {
